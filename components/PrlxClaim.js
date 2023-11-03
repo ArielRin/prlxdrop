5e0c951fed612196e97481ffabdfe96186a4ff09
Cheyne Hayworth <degenftm@gmail.com>
2023-11-04 10:14:25 +1100

    ui and temp claim from nft contract for testing

--- components/PrlxClaim.js
+++ components/PrlxClaim.js
@@ -1,66 +1,124 @@
 import style from '../styles/ClaimCard.module.css';
-import truncateEthAddress from 'truncate-eth-address';
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 
 const PrlxClaim = () => {
   const [web3, setWeb3] = useState(null);
+  const [mintedTokens, setMintedTokens] = useState(0);
+  const [totalSupply, setTotalSupply] = useState(0);
+
   useEffect(() => {
     async function initWeb3() {
       if (window.ethereum) {
-        const web3Instance = new Web3(window.ethereum);
-        await window.ethereum.enable(); // Request user permission
-        setWeb3(web3Instance);
+        try {
+          await window.ethereum.enable();
+          const web3Instance = new Web3(window.ethereum);
+          setWeb3(web3Instance);
+        } catch (error) {
+          console.error('User denied access to the wallet or other error:', error);
+        }
+      } else {
+        console.error('Web3 is not initialized. Make sure you have a compatible wallet and network connected.');
       }
     }
+
     initWeb3();
+    updateTotalSupply(); // Fetch the initial total supply and update it when the component mounts
   }, []);
 
-  // Function to handle the "Enter" button click
-  const handleMakeClaim = async () => {
+  const updateTotalSupply = async () => {
     if (web3) {
-      const contractAddress = '0xd7EA92A63371cC9324E9Fde3F69c7aDfBd77BAEc'; // Replace with your contract address
-      const contractABI = [];
+      const contractAddress = '0x8911ADF00521e1886162a9BA87699fb0D32312A0';
+      const contractABI = [
+        // ... (Your contract ABI)
+        {
+          "inputs": [],
+          "name": "totalSupply",
+          "outputs": [
+            {
+              "internalType": "uint256",
+              "name": "",
+              "type": "uint256"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+      ]; // Your contract ABI
 
       const claimContract = new web3.eth.Contract(contractABI, contractAddress);
-      const accounts = await web3.eth.getAccounts();
-
-      try {
-        await claimContract.methods.enter().send({
-          from: accounts[0],
-          value: web3.utils.toWei('100', 'ether'), // Sending 100 PWR, adjust as needed
-        });
-        console.log('Tokens Claim successfully');
-      } catch (error) {
-        console.error('Error claiming', error);
-      }
-    } else {
-      console.error('Web3 is not initialized. Make sure you have a compatible wallet and network connected.');
+      const totalSupply = await claimContract.methods.totalSupply().call();
+      setTotalSupply(totalSupply);
     }
   };
 
+  const claimTokens = async () => {
+    if (web3) {
+      try {
+        const contractAddress = '0x8911ADF00521e1886162a9BA87699fb0D32312A0';
+        const contractABI = [
+          // ... (Your contract ABI)
+          {
+            "inputs": [
+              {
+                "internalType": "uint256",
+                "name": "_mintAmount",
+                "type": "uint256"
+              }
+            ],
+            "name": "mint",
+            "outputs": [],
+            "stateMutability": "payable",
+            "type": "function"
+          },
+        ]; // Your contract ABI
 
-    useEffect(() => {// Fetch current lottery ID when the component mounts
-    }, [web3]);
+        const claimContract = new web3.eth.Contract(contractABI, contractAddress);
+        const accounts = await web3.eth.getAccounts();
+
+        // Send a transaction to mint tokens
+        await claimContract.methods.mint(1).send({
+          from: accounts[0],
+        });
+
+        // Update the mintedTokens count
+        setMintedTokens(mintedTokens + 1);
+
+        console.log('Token minted successfully');
+      } catch (error) {
+        console.error('Error minting the token', error);
+      }
+    }
+  };
+
+  // Calculate the remaining supply
+  const remainingSupply = totalSupply - mintedTokens;
 
   return (
     <div className={style.wrapper}>
       <div className={style.title}>
         Peet DeFi - Parallex Migration
       </div>
-        <div className={style.title}>
-          {' '}
-        </div>
+      <div className={style.title}></div>
       <div className={style.paragragh}>
-       Peet Defi Snapshot Taken Claim your PRLX Tokens below
+        Peet Defi Snapshot Taken Claim your PRLX Tokens below
+      </div>
+      <div className={style.paragragh}></div>
+      <div className={style.btn} onClick={claimTokens}>
+        Claim your PRLX
+      </div>
+
+      <div className={style.Spacing20}></div>
+      <div className={style.paragragh}>
+        PRLX Total Supply: {totalSupply}
       </div>
       <div className={style.paragragh}>
+        Total PRLX Claimed: {mintedTokens}
       </div>
-      <div className={style.btn} onClick={handleMakeClaim}>
-        Claim PRLX
+      <div className={style.paragragh}>
+        Remaining PRLX Supply: {remainingSupply}
       </div>
     </div>
-
   );
 };
 
